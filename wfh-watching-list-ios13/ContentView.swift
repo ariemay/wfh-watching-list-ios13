21e7eacefefa12fc4398d9c397c8c2c4340c6fa9
Arie May Wibowo <ariemayw@gmail.com>
2020-07-02 13:32:53 +0700

    Initial Commit

---
+++ wfh-watching-list-ios13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wfh-watching-list-ios13
+//
+//  Created by Arie May Wibowo on 02/07/20.
+//  Copyright © 2020 Arie May Wibowo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
